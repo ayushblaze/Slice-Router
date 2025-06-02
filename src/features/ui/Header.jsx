5775786cf76fb0663aea40f4c786ba503836dc4a
ayushblaze <ayushmoca@gmail.com>
2025-06-02 23:11:24 +0530

    feat(header): add styles

--- src/features/ui/Header.jsx
+++ src/features/ui/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../order/SearchOrder";
+import Username from "../user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to='/'>SliceRouter</Link>
+    <header className="bg-yellow-500 uppercase px-4 py-3 border-b border-stone-500">
+      <Link to='/' className="tracking-widest">SliceRouter</Link>
       <SearchOrder />
-      <p>Ayush Singh</p>
+      <Username />
     </header>
   )
 }
