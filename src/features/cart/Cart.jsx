d03c37eedc7d52a0e503fddb3c30f946fcbd6f3f
ayushblaze <ayushmoca@gmail.com>
2025-06-05 22:23:25 +0530

    feat(redux): manage user state using rtk

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import LinkButton from '../ui/LinkButton';
 import Button from '../ui/Button';
 import CartItem from './CartItem';
+import { useSelector } from 'react-redux';
 
 const fakeCart = [
   {
@@ -27,6 +28,7 @@ const fakeCart = [
 ];
 
 function Cart() {
+  const username = useSelector(state => state.user.username);
   const cart = fakeCart;
 
   return (
@@ -35,10 +37,10 @@ function Cart() {
         &larr; Back to menu
       </LinkButton>
 
-      <h2 className='mt-7 text-xl font-semibold'>Your cart, %NAME%</h2>
+      <h2 className='mt-7 text-xl font-semibold'>Your cart, {username}</h2>
 
       <ul className='divide-y divide-stone-200 border-b mt-3'>
-        {cart.map((item) => <CartItem item={item} key={item.key} />)}
+        {cart.map((item) => <CartItem item={item} key={item.pizzaId} />)}
       </ul>
 
       <div className='mt-6 space-x-2'>
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,14 @@
+import { useSelector } from "react-redux";
+
 function Username() {
+  const username = useSelector(state => state.user.username);
+  console.log("from redux:", username);
+
+  if (!username) return null;
+
   return (
     <div className="text-sm font-semibold hidden md:block">
-      Ayush Singh
+      {username}
     </div>
   )
 }
